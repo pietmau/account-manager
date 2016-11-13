c80ffb495f4a2cd3740277f08e137c566450f1af
Maurizio Pietrantuono <maurizio.pietrantuono@gmail.com>
2016-11-13 01:03:44 +0000

    Authenticates, need to return to authenticator

--- AccountManager/app/src/main/java/com/pietrantuono/accountmanager/AuthenticatorActivity.java
+++ AccountManager/app/src/main/java/com/pietrantuono/accountmanager/AuthenticatorActivity.java
@@ -1,17 +1,23 @@
 package com.pietrantuono.accountmanager;
 
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.TextView;
 
+import net.openid.appauth.AuthState;
+import net.openid.appauth.AuthorizationException;
 import net.openid.appauth.AuthorizationRequest;
+import net.openid.appauth.AuthorizationResponse;
 import net.openid.appauth.AuthorizationService;
 import net.openid.appauth.AuthorizationServiceConfiguration;
+import net.openid.appauth.TokenResponse;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -20,6 +26,9 @@ import java.util.Map;
  * Created by Maurizio Pietrantuono, maurizio.pietrantuono@gmail.com
  */
 public class AuthenticatorActivity extends AppCompatActivity {
+    private static final String USED_INTENT = "USED_INTENT";
+    public static final String SHARED_PREFERENCES_NAME = "prefs";
+    public static final String AUTH_STATE = "auth";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -27,7 +36,9 @@ public class AuthenticatorActivity extends AppCompatActivity {
         TextView textView = new TextView(AuthenticatorActivity.this);
         textView.setText("FFFFFFF");
         setContentView(textView);
-        authenticate();
+        if (!intentContanisAuth(getIntent())) {
+            authenticate();
+        }
     }
 
     @Override
@@ -68,9 +79,54 @@ public class AuthenticatorActivity extends AppCompatActivity {
 //        }
 
         AuthorizationRequest request = builder.build();
-        String action = "com.google.codelabs.appauth.HANDLE_AUTHORIZATION_RESPONSE";
+        String action = "com.pietrantuono.accountmanager.HANDLE_AUTHORIZATION_RESPONSE";
         Intent postAuthorizationIntent = new Intent(action);
         PendingIntent pendingIntent = PendingIntent.getActivity(AuthenticatorActivity.this, request.hashCode(), postAuthorizationIntent, 0);
         authorizationService.performAuthorizationRequest(request, pendingIntent);
     }
+
+    private boolean intentContanisAuth(@Nullable Intent intent) {
+        if (intent != null && intent.getAction() != null) {
+            String action = intent.getAction();
+            switch (action) {
+                case "com.pietrantuono.accountmanager.HANDLE_AUTHORIZATION_RESPONSE":
+                    if (!intent.hasExtra(USED_INTENT)) {
+                        handleAuthorizationResponse(intent);
+                        intent.putExtra(USED_INTENT, true);
+                    }
+                    return true;
+                default:
+                    return false;
+            }
+        }
+        return false;
+    }
+
+    private void handleAuthorizationResponse(@NonNull Intent intent) {
+        AuthorizationResponse response = AuthorizationResponse.fromIntent(intent);
+        AuthorizationException error = AuthorizationException.fromIntent(intent);
+        final AuthState authState = new AuthState(response, error);
+        if (response != null) {
+            AuthorizationService service = new AuthorizationService(this);
+            service.performTokenRequest(response.createTokenExchangeRequest(), new AuthorizationService.TokenResponseCallback() {
+                @Override
+                public void onTokenRequestCompleted(@Nullable TokenResponse tokenResponse, @Nullable AuthorizationException exception) {
+                    if (exception != null) {
+                    } else {
+                        if (tokenResponse != null) {
+                            authState.update(tokenResponse, exception);
+                            persistAuthState(authState);
+                        }
+                    }
+                }
+            });
+        }
+    }
+    private void persistAuthState(@NonNull AuthState authState) {
+        getSharedPreferences(SHARED_PREFERENCES_NAME, Context.MODE_PRIVATE).edit()
+                .putString(AUTH_STATE, authState.toJsonString())
+                .commit();
+    }
+
+
 }
